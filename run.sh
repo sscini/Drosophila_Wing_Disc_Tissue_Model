221e4d439e052dc0f84701a2ac88a5fa2f7f6041
Stephen Cini <114932899+sscini@users.noreply.github.com>
2026-04-15 06:52:06 -0400

    Revert "Ran small test job, successful"
    This reverts commit f544a5908577e15654ca3731149cbdf9fc6ba476.

--- run.sh
+++ run.sh
@@ -3,12 +3,12 @@
 #to run without script you must type this line into the terminal:
 setenv LD_LIBRARY_PATH "${LD_LIBRARY_PATH}:/afs/crc.nd.edu/x86_64_linux/m/matlab/R2017b/extern/bin/glnxa64/"
 
-#$ -M  scini@nd.edu# Email address for job notification
+#$ -M  nsher012@ucr.edu# Email address for job notification
 #$ -m  abe		 # Send mail when job begins, ends and aborts
 #$ -q  gpu 	 # Specify queue
 #$ -l gpu_card=1
 #s -pe smp 6         #specifies threads??? maybe
-#$ -N  "build_test_040826" # Specify job name
+#$ -N  "dsp_test_2_circular" # Specify job name
 #$ -t 1       #specify number of data input files
 
 
@@ -18,9 +18,9 @@ module purge
 module load cuda
 
 echo -n "It is currently: ";date
-echo -n "I am logged on as scini";whoami
+echo -n "I am logged on as nsher012";whoami
 echo -n "This computer is called Lernie";hostname
-# echo -n "I am currently in the directory /rhome/scini/Dsp_wing_eversion/07-03-25-codes_to_change/";pwd
+echo -n "I am currently in the directory /rhome/nsher012/Dsp_wing_eversion/07-03-25-codes_to_change/";pwd
 
 
 ./tissue-model  -solve_time=5 -dt=0.01 $data[${SGE_TASK_ID}]
